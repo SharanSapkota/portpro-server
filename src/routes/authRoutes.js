7c90fe9abbdc2325971c9ccd6037df90cf2ce883
sharan <sharan@botsplash.com>
2022-07-18 02:34:29 +0545

    Add controller functionality

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,40 +1,17 @@
-const router = require("express").Router();
-const passport = require("passport");
+const router                                = require("express").Router();
+const passport                              = require("passport");
+const { authenticate, failedLogin, logout } = require("../controllers/twitter");
+
 const CLIENT_HOME_PAGE_URL = "http://localhost:3000";
 
-// when login is successful, retrieve user info
-router.get("/login/success", (req, res) => {
-    if (req.user) {
-        res.status(200).json({
-            success: true,
-            message: "user has successfully authenticated",
-            user: req.user,
-            cookies: req.cookies
-        });
-    }
-    else {
-        res.status(404).json({success: false, message: 'User not found!'});
-    }
-});
+router.get("/login/success", authenticate);
 
-// when login failed, send failed msg
-router.get("/login/failed", (req, res) => {
-    res.status(401).json({
-        success: false,
-        message: "user failed to authenticate."
-    });
-});
+router.get("/login/failed", failedLogin);
 
-// When logout, redirect to client
-router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect(CLIENT_HOME_PAGE_URL);
-});
+router.get("/logout", logout);
 
-// auth with twitter
 router.get("/twitter", passport.authenticate("twitter"));
 
-// redirect to home page after successfully login via twitter
 router.get("/twitter/redirect",
     passport.authenticate("twitter", {
         successRedirect: CLIENT_HOME_PAGE_URL,
